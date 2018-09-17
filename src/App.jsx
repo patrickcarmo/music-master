56100008390920f3d881c1868a5d0affc84463f4
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-17 13:55:54 +0100

    Athentication and basic artist search spotify

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,45 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 import './App.css';
-
+import Authorization from './authorizarion'
 class App extends Component{
+
+    state = [];
 
     constructor(props) {
         super(props);
-        console.log(props);
+        this.state = {
+            query: ''
+        };
+        console.log(this.state);
+    }
+
+    search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+        const accessToken = Authorization.SPOTIFY;
+
+        let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+
+        var myOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions)
+            .then(response => response.json())
+            .then(json => console.log())
+    }
+
+    searchKeyPress(event) {
+        if (event.key === 'Enter') {
+            this.search();
+        }
     }
 
     render() {
@@ -19,8 +52,11 @@ class App extends Component{
                         <FormControl
                             type="text"
                             placeholder="Search for an Artist.."
+                            value={this.state.query}
+                            onChange={event => this.setState({ query: event.target.value })}
+                            onKeyPress={event => this.searchKeyPress(event)}
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search" />
                         </InputGroup.Addon>
                     </InputGroup>
